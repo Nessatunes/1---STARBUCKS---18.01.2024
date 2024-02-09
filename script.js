da571d83392a3b2000171bcd6dc4f51a4a5f259a
Nessatunes <essatunes@gmail.com>
2024-02-09 19:04:58 -0300

    alteraçoes

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function trocaImagem(src, cor) {
+    let imgCopo = document.querySelector(".starbucks");
+    let circulo = document.querySelector(".circulo");
+  
+    imgCopo.src = src;
+    circulo.style.background = cor;
+  }
+
+
+
+
